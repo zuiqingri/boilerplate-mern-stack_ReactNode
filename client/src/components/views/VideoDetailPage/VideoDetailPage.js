5071d2cebaeaa541568ed0fec5c65bf296428ad2
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-11-10 11:15:35 +0800

    Subscribe update

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -58,6 +58,7 @@ function VideoDetailPage(props) {
 
 
      if (VideoDetail.writer) {
+         const subscribeButton=VideoDetail.writer._id !== localStorage.getItem('userId') && <Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')}/>
         return (
             <Row>
                 <Col lg={18} xs={24}>
@@ -65,7 +66,7 @@ function VideoDetailPage(props) {
                         <video style={{ width: '100%' }} src={`http://localhost:5000/${VideoDetail.filePath}`} controls></video>
 
                         <List.Item
-                            actions={[<Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')}/>]}
+                            actions={[subscribeButton]}
                             // actions={[<LikeDislikes video videoId={videoId} userId={localStorage.getItem('userId')}  />, <Subscriber userTo={Video.writer._id} userFrom={localStorage.getItem('userId')} />]}
                         >
                             <List.Item.Meta
